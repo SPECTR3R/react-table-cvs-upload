5a761a66386682b75b96c15cd7b0cbf7b96c85c5
Armando del Río <d-rio@outlook.com>
2021-03-05 10:17:01 -0600

    Add editable table component

---
+++ components/table/CVSUploader.js
@@ -0,0 +1,72 @@
+import { useRef, useState } from 'react'
+import { CSVReader } from 'react-papaparse'
+import _ from 'lodash'
+
+const CSVUploader = ({ setColumns, setData, error, setError }) => {
+  const clearButtonRef = useRef(null)
+  const [isFileOn, setIsFileOn] = useState(false)
+
+  const handleOnDrop = (fileData, { type }) => {
+    try {
+      setIsFileOn(false)
+      setError(null)
+      console.log(type)
+      if (type !== 'text/csv') {
+        throw new Error('Archivo invalido')
+      }
+      const headers = fileData.shift().data.map((elem, idx) => ({ Header: elem, accessor: `${idx}` }))
+      const values = fileData.map((rowElem) => ({ ...rowElem.data })).filter((elem) => Object.keys(elem).length > 1)
+      setColumns(headers)
+      setData(values)
+      setIsFileOn(true)
+    } catch (err) {
+      setError(err)
+      console.log(err)
+    }
+  }
+
+  const handleOnError = (err) => {
+    console.log(err)
+    setError(true)
+  }
+
+  const handleRemoveFile = (e) => {
+    setIsFileOn(false)
+    setError(null)
+    if (clearButtonRef.current) {
+      clearButtonRef.current.removeFile(e)
+      setColumns(null)
+      setData(null)
+    }
+  }
+
+  return (
+    <div>
+      <CSVReader ref={clearButtonRef} onDrop={handleOnDrop} onError={handleOnError}>
+        <span>Cliquea aquí o arrastra tu archivo .CVS a esta región para subir.</span>
+      </CSVReader>
+
+      {error?.message && <div>Error: {error.message}</div>}
+
+      <button
+        type="button"
+        onClick={handleRemoveFile}
+        w="auto"
+        style={{ visibility: handleElementVisibility(error, isFileOn) }}
+      >
+        Eliminar archivo
+      </button>
+    </div>
+  )
+}
+
+export default CSVUploader
+
+function handleElementVisibility(...rest) {
+  console.log(rest)
+  const isAnyflagTruthy = rest.some((flag) => !!flag)
+
+  const ret = isAnyflagTruthy ? 'visible' : 'hidden'
+
+  return ret
+}
---
+++ components/table/EditableTable.js
@@ -0,0 +1,178 @@
+import React from 'react'
+import { useTable, useSortBy, usePagination } from 'react-table'
+import EditableCell from './EditableCell.js'
+
+// Be sure to pass our updateMyData and the skipPageReset option
+function Table({ columns, data, updateMyData, skipPageReset }) {
+  // For this example, we're using pagination to illustrate how to stop
+  // the current page from resetting when our data changes
+  // Otherwise, nothing is different here.
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn: {
+        Cell: EditableCell,
+      },
+      // use the skipPageReset option to disable page resetting temporarily
+      autoResetPage: !skipPageReset,
+      // updateMyData isn't part of the API, but
+      // anything we put into these options will
+      // automatically be available on the instance.
+      // That way we can call this function from our
+      // cell renderer!
+      updateMyData,
+    },
+    useSortBy,
+    usePagination
+  )
+
+  React.useEffect(() => {
+    setPageSize(20)
+  }, [setPageSize])
+
+  // Render the UI for your table
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps)}>
+                  {column.render('Header')}
+                  {column.isSorted && <span>{column.isSortedDesc ? ' ↓' : ' ↑'}</span>}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                ))}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button type="button" onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>
+        <button type="button" onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>
+        <button type="button" onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>
+        <button type="button" onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>
+        <span>
+          Página
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>
+        </span>
+        <span>
+          &nbsp;| Ir a página:
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageToGo = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(pageToGo)
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[20, 40, 60, 50].map((individualPageSize) => (
+            <option key={individualPageSize} value={individualPageSize}>
+              Show {individualPageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
+  )
+}
+
+function EditableTable({ rawCols, rawData, setUpdatedData = null }) {
+  const columns = React.useMemo(() => [...rawCols], [rawCols])
+
+  const [data, setData] = React.useState(() => rawData)
+
+  const [originalData] = React.useState(rawData)
+  const [skipPageReset, setSkipPageReset] = React.useState(false)
+
+  // We need to keep the table from resetting the pageIndex when we
+  // Update data. So we can keep track of that flag with a ref.
+
+  // When our cell renderer calls updateMyData, we'll use
+  // the rowIndex, columnId and new value to update the
+  // original data
+  const updateMyData = (rowIndex, columnId, value) => {
+    // We also turn on the flag to not reset the page
+    setSkipPageReset(true)
+    setData((old) =>
+      old.map((row, index) => {
+        if (index === rowIndex) {
+          return {
+            ...old[rowIndex],
+            [columnId]: value,
+          }
+        }
+        return row
+      })
+    )
+  }
+
+  // After data chagnes, we turn the flag back off
+  // so that if data actually changes when we're not
+  // editing it, the page is reset
+  React.useEffect(() => {
+    if (setUpdatedData !== null) setUpdatedData(data)
+    setSkipPageReset(false)
+  }, [data, setUpdatedData])
+
+  // Let's add a data resetter/randomizer to help
+  // illustrate that flow...
+  const resetData = () => setData(originalData)
+
+  return (
+    <>
+      <button type="button" onClick={resetData}>
+        Resetear
+      </button>
+      <Table columns={columns} data={data} updateMyData={updateMyData} skipPageReset={skipPageReset} />
+    </>
+  )
+}
+
+export default EditableTable
