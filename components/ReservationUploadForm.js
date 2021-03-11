17d5f7fb183c44a5259a93ea42c0a8a9e673eac3
Armando del Río <d-rio@outlook.com>
2021-03-10 19:23:33 -0600

    Finish loading cache on render

--- components/ReservationUploadForm.js
+++ components/ReservationUploadForm.js
@@ -58,36 +58,49 @@ const ReservationUploadForm = ({ selectableOptions, reservationsData }) => {
     {
       label: 'Nombre y Apellido',
       name: 'firstName',
+      required: true,
     },
   ]
 
   const [selectedOptions, setSelectedOptions] = useState({})
-  const [reservationNameFields, setReservationNameFields] = useState(inputFields)
+  const [reservationNameFields, setReservationNameFields] = useState([...separatedNameInputFields, ...inputFields])
   const [isNameSeparated, setIsNameSeparated] = useState('true')
 
+  // useEffect(() => {
+  //   if (!_.isEmpty(selectedOptions)) return
+  //   const cache = getDataCache('DATA_CACHE')
+  //   const cachedSelectedOptions = cache?.data?.inputValues?.selectedOptions
+  //   const cachedIsNameSeparated = cache?.data?.inputValues?.isNameSeparated
+
+  //   if (_.isEmpty(cachedSelectedOptions)) return
+  //   setSelectedOptions((prev) => {
+  //     const appendOtions = { ...cachedSelectedOptions, ...prev }
+  //     delete appendOtions.firstName
+  //     delete appendOtions.lastName
+  //     return appendOtions
+  //   })
+  // }, [selectedOptions])
+
   useEffect(() => {
-    if (!_.isEmpty(selectedOptions)) return
     const cache = getDataCache('DATA_CACHE')
-    const cacheData = cache?.data?.inputValues?.selectedOptions
-    if (_.isEmpty(cacheData)) return
-    setSelectedOptions((prev) => {
-      const appendOtions = { ...cacheData, ...prev }
-      delete appendOtions.firstName
-      delete appendOtions.lastName
-      return appendOtions
-    })
-  }, [selectedOptions])
+    const cachedSelectedOptions = cache?.data?.inputValues?.selectedOptions
+    const cachedIsNameSeparated = cache?.data?.inputValues?.isNameSeparated
+    console.log(cachedSelectedOptions, cachedIsNameSeparated)
+    setSelectedOptions(cachedSelectedOptions)
+    setIsNameSeparated(cachedIsNameSeparated)
+  }, [setSelectedOptions, setIsNameSeparated])
 
   const handleSelectChange = (e) => {
     const { name, value } = e.target
     setSelectedOptions((prevInputData) => ({ ...prevInputData, [name]: value }))
+    return setDataToCache({ selectedOptions, isNameSeparated })
   }
 
   const handleFormSubmit = (e) => {
     e.preventDefault()
     // console.log(reservationsData, 'holi')
     // console.log(selectedOptions, 'holi')
-    setDataToCache({ selectedOptions })
+    // setDataToCache({ selectedOptions, isNameSeparated })
     console.log('selected', selectedOptions)
     // console.log(columns)
     // const result = basariReservationGenerator(reservationsData, selectedOptions)
@@ -97,10 +110,13 @@ const ReservationUploadForm = ({ selectableOptions, reservationsData }) => {
   const handleRadioChange = (e) => {
     if (e.target.value === 'true') {
       setIsNameSeparated('true')
-      return setReservationNameFields([...separatedNameInputFields, ...inputFields])
+      setReservationNameFields([...separatedNameInputFields, ...inputFields])
+
+      return setDataToCache({ selectedOptions, isNameSeparated: 'true' })
     }
     setIsNameSeparated('false')
-    return setReservationNameFields([...connectedNameInputFields, ...inputFields])
+    setReservationNameFields([...connectedNameInputFields, ...inputFields])
+    return setDataToCache({ selectedOptions, isNameSeparated: 'false' })
   }
 
   return (
